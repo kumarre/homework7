fd001bfe0369ada9f24260a7dcbbc9391d12968c
kumarre <kumarre@umich.edu>
2020-11-25 15:25:50 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,28 +1,30 @@
 function gettingJSON(){
     //Display the forecast
     // Your code here.
+    document.querySelector("#forecast").style["display"] = "block";
 
     //Set default location if one isn't provided
-    let location;
+    let place = "ann arbor";
+    let country = "us";
 
     // Your code here.
-
-    location = document.querySelector("#location").value;
+    let location = document.querySelector("#location").value;
     if (location.length == 0) {
-        location = "ann arbor, us";
+        location = "ann arbor";
+    }
+    else if (location.split(",").length == 1) {
+        place = location;
+    }
+    else {
+        let loc_array = location.split(",");
+        place = loc_array[0];
+        country = loc_array[1];
     }
     
-    console.log("Location is : " + location);
+    console.log("Location is : " + place + ", " + country);
 
-    let loc_array = location.split(",");
 
-    let item1 = loc_array[0].toLowerCase();
-    // console.log("item 1 is: "+item1);
-
-    let item2 = loc_array[1].toLowerCase();
-
-    item2 = item2.trim();
-
+  
     //set default temperature format if one isn't provided
     let format;
 
@@ -35,20 +37,22 @@ function gettingJSON(){
     }
     console.log("Format is :" + format);
 
+
+
     //set the query  
     let query;
     // Your code here.
     let appid =  "cbdfac893fa4a8707a6ce38a25f57314";
 
-    console.log("item1 is city name: " + isNaN(item1));
-    if (isNaN(item1)) {
+    console.log("place is city name: " + isNaN(place));
+    if (isNaN(place)) {
         //city name
-        query = 'https://api.openweathermap.org/data/2.5/weather?q='+item1+','+item2+'&units='+format+'&appid='+appid;
+        query = 'https://api.openweathermap.org/data/2.5/weather?q='+place+','+country+'&units='+format+'&appid='+appid;
 
     }
     else {
         //zip code
-        query = 'https://api.openweathermap.org/data/2.5/weather?zip='+item1+','+item2+'&units='+format+'&appid='+appid;        
+        query = 'https://api.openweathermap.org/data/2.5/weather?zip='+place+','+country+'&units='+format+'&appid='+appid;        
     }
 
     console.log("Query is :" + query);
@@ -69,11 +73,10 @@ function gettingJSON(){
         loc = json["name"];
         temp = json["main"]["temp"];
         description = json["weather"][0]["description"];
-        tempImg = "http://openweathermap.org/img/wn/"+json["weather"][0]["icon"]+"@2x.png";
+        tempImg = "http://openweathermap.org/img/wn/"+json["weather"][0]["icon"]+".png";
 
         console.log(JSON.stringify(json));
-        // Change the div with the weather information so that it is now visible.
-        document.querySelector("#forecast").style["display"] = "block";
+
         // Update the value for the location.  You should show the CITY name, even if the zip code is entered.
         document.querySelector("#loc").innerHTML = loc;
 
